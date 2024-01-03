b07aa25eddcbd94630554526fc349eebb346285f
comision24 <comision24formar@gmail.com>
2024-01-02 21:17:43 -0300

    KITCHENING maquetacion y style view mobile

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const port = 3030;
+
+
+app.use(express.static('public'))
+
+app.get('/',(req,res) => {
+  res.sendFile(path.join(__dirname,'./views/home.html'))
+})
+
+
+app.listen(port,() => console.log(`http://localhost:${port}`))
+
