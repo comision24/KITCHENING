32027bd792f9de96d31a840e432bead5eea16d26
ema201217@gmail.com <ema201217@gmail.com>
2024-06-10 21:09:33 -0300

    feat: add list product in dashboard

--- dashboard/src/pages/Product.jsx
+++ dashboard/src/pages/Product.jsx
@@ -1,48 +1,152 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { TableHead } from "../components/Products/TableHead";
-import { TableRow } from "../components/Products/TableRow";
+import React, { useEffect, useState } from "react";
+
+import { DataGrid } from "@mui/x-data-grid";
+import { Container } from "@mui/material";
 
 const Product = () => {
-  const movies = [
+  const [statesProducts, setStatesProducts] = useState({
+    loading: true,
+    products: [],
+    error: "",
+  });
+
+  const [dataGrid, setDataGrid] = useState({
+    columns: [],
+    rows: [],
+  });
+
+  useEffect(() => {
+    const endpoint = "http://localhost:3030/api/products?limit=10000";
+    const getProducts = async () => {
+      try {
+        const {
+          ok,
+          data = [],
+          msg = null,
+        } = await fetch(endpoint).then((res) => res.json());
+
+        if (!ok) throw new Error(msg);
+
+        ok &&
+          setStatesProducts({
+            ...statesProducts,
+            products: data,
+            loading: false,
+          });
+      } catch (error) {
+        setStatesProducts({
+          ...statesProducts,
+          error: error.message,
+        });
+      }
+    };
+
+    getProducts();
+  }, []);
+
+  useEffect(() => {
+    /* console.log(statesProducts.products); */
+    const dataObjProduct = Object.entries(
+      statesProducts.products.length ? statesProducts.products[0] : {}
+    );
+
+    const listWrite = ["id", "title", "price", "description"];
+    const headerNameTable = {id: "ID", title: "TITULO", price: "PRECIO", description: "DESCRIPCIÓN"}
+    const columnsFormat = dataObjProduct
+      .filter(([key, value]) => listWrite.includes(key))
+      .map(([key, value]) => {
+          // [ "id" , 1 ] iteración 1
+          // [ "title" , "COMIDA 1" ] iteración 2
+          // [ "price" , 100 ] iteración 3
+          // [ "description" , "asdjhbdskhbjdaskjhbadskjhb" ] iteración 4
+
+        return {
+          field: key,
+          headerName: headerNameTable[key], // headerNameTable["description"] -> value -> asdjhbdskhbjdaskjhbadskjhb
+          width: 150,
+          type: typeof value,
+          editable: true
+        };
+
+        /* 
+        const columnsProduct = [
+        {
+          field: "id",
+          headerName: "ID",
+          width: 150,
+          type: "number",
+        },
+        {
+          field: "title",
+          headerName: "Titulo",
+          width: 150,
+          type: "string",
+        },
+      ];
+      */
+      });
+
+    // [[key, value], [key, value]]
+    const rowsFormat = [];
+
+    statesProducts.products.forEach((product) => {
+      const objData = {};
+      Object.entries(product).forEach(([key, value]) => {
+        if (listWrite.includes(key)) {
+          objData[key] = value;
+        }
+      });
+      rowsFormat.push(objData);
+    });
+
+    /* const rowsFormat = statesProducts.products
+    .reduce((acum, product) => {
+      const objData = {};
+      Object.entries(product).forEach(([key, value]) => {
+        if (listWrite.includes(key)) {
+          objData[key] = value;
+        }
+      });
+      acum.push(objData);
+    }, []); */
+
+
+    setDataGrid({
+      rows: rowsFormat,
+      columns: columnsFormat,
+    });
+
+    console.log(columnsFormat);
+  }, [statesProducts.products]);
+
+/*   const rowProduct = [
     {
       id: 1,
-      title: "Billy Elliot",
-      duration: 123,
-      rating: 5,
-      genre: ["Drama", "Comedia"],
-      awards: 2,
-    },
-    {
-      id: 2,
-      title: "Alicia en el país de las maravillas",
-      duration: 142,
-      rating: 4.8,
-      genre: ["Drama", "Acción", "Comedia"],
-      awards: 3,
+      title: "COCTELERÍA Y BEBIDAS",
+      price: 1000,
+      description: "dasasaddasadsads ad sasdasd",
     },
   ];
 
-  const dataTableHead = ["Titulo", "Duración", "Rating", "Género", "Premios"];
+   */
 
   return (
     <>
       <h1>TODAS LOS PRODUCTOS</h1>
 
-      <div className="border p-1 m-3">
-        <table className="table table-bordered m-4">
-          <TableHead items={dataTableHead} />
-
-          <tbody>
-            {movies.map((movie, i) => (
-              /*  <TableRow key={i} title={movie.title} duration={movie.duration} rating={movie.rating} genre={movie.genre} awards={movie.awards}/> */
-              <TableRow key={i} {...movie} />
-            ))}
-          </tbody>
-
-          <TableHead items={dataTableHead} />
-        </table>
-      </div>
+      <Container maxWidth={400} style={{ height: 400 }}>
+        <DataGrid
+          rows={dataGrid.rows}
+          columns={dataGrid.columns}
+          initialState={{
+            pagination: {
+              paginationModel: { page: 0, pageSize: 5 },
+            },
+          }}
+          pageSizeOptions={[5, 10]}
+          checkboxSelection
+        />
+      </Container>
     </>
   );
 };
--- dashboard/src/pages/Users.jsx
+++ dashboard/src/pages/Users.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 
 const Users = (props) => {
+  const [states, setStates] = useState({
+    loading: true,
+    users: [],
+    error: ""
+  })
+
+  useEffect(() => {
+    const endpoint = "http://localhost:3030/api/users?limit=10000";
+    const getUsers = async () => {
+      try {
+        const { ok, data = [], msg = null } = await fetch(endpoint).then((res) => res.json());
+
+        if(!ok) throw new Error(msg)
+
+          console.log(data)
+        ok && setStates({
+          ...states,
+          users: data,
+          loading: false
+        })
+      } catch (error) {
+        setStates({
+          ...states,
+          error: error.message
+        })
+      }
+    };
+
+    getUsers();
+
+  }, []);
+
   return <h1>TODAS LOS USUARIOS</h1>;
 };
 
